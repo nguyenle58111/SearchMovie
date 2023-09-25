94eeb1e95cd52071bc7716b81c7f5eb8d2199dc5
nguyen <127307734+nguyenle58111@users.noreply.github.com>
2023-09-25 21:22:49 +0700

    khoang cach

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -15,7 +15,7 @@ function MovieCard({ allMovieData, loading }) {
                             <div key={index} className="p-2 md:w-1/4 w-full">
                                 <div className="bg-[#40407a] p-3 rounded-2xl shadow-lg hover:-translate-y-1 border-2 border-gray-200">
                                     <img className='rounded-lg w-full h-full mb-2' src={Poster} alt="" />
-                                    <h2 className='text-xl text-white font-bold'>{Title}</h2>
+                                    <h2 className='text-xl text-white font-bold mb-2'>{Title}</h2>
                                     <h2 className='text-lg text-white mb-2'>Year : {Year}</h2>
                                     <h2 className='text-lg text-white mb-2'>Type : {Type}</h2>
                                 </div>
